ce5c292f8cd4f31ceee8012055c6f6c0ac957d4b
Fabio Varesano <fax8@13637.no-reply.drupal.org>
2007-01-14 16:35:44 +0000

    Added support for busy indicator on video uploads.

--- plugins/video_upload/video_upload.js
+++ plugins/video_upload/video_upload.js
@@ -8,5 +8,22 @@
  * @author Fabio Varesano <fvaresano at yahoo dot it>
 */
 
+/**
+ * Hide the node form and show the busy div
+*/
+Drupal.video_upload_hide = function () {
+  $('#node-form').hide();
+  $("#sending").show();
+}
 
+/**
+ * Attaches the upload behaviour to the video upload form.
+ */
+Drupal.video_upload = function() {
+  $('#node-form').submit(video_upload_hide);
+}
 
+// Global killswitch
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.video_upload);
+}
