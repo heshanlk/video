cd355d137ff231091774b87050ffd5ec76d9ef4d
Fabio Varesano <fax8@13637.no-reply.drupal.org>
2007-01-14 14:59:06 +0000

    Let's prepare for video upload status bar.

---
+++ plugins/video_upload/video_upload.js
@@ -0,0 +1,12 @@
+// $Id$
+/**
+ * @file
+ * Javascript functions for busy status on video uploads
+ * 
+ * TODO: Support AJAX Uploads :-)
+ *
+ * @author Fabio Varesano <fvaresano at yahoo dot it>
+*/
+
+
+
--- video.js
+++ video.js
@@ -36,3 +36,6 @@ if (document.all) {
   }
 
 }
+
+
+
