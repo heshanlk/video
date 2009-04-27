3433e80f20ea08910b06dd7bb629165763187538
The Great Git Migration <tggm@no-reply.drupal.org>
2009-04-27 09:48:40 +0000

    This commit was manufactured as part of Drupal's Great Git Migration to create branch 'DRUPAL-6--1'.
    Cherrypick from master 2009-04-27 09:48:39 UTC Heshan Wanigasooriya <heshanmw@gmail.com> 'adding includes files':
        includes/apiclient.inc
        includes/common.inc
        types/video_google/video_google.info
        types/video_google/video_google.module
        types/video_upload/busy.gif
        types/video_upload/video_upload.info
        types/video_upload/video_upload.js
        types/video_upload/video_upload.module
        types/video_url/video_url.info
        types/video_url/video_url.module
        types/video_youtube/video_youtube.info
        types/video_youtube/video_youtube.module

---
+++ types/video_upload/video_upload.js
@@ -0,0 +1,44 @@
+// $Id$
+/**
+ * @file
+ * Javascript functions for busy status on video uploads
+ *
+ * TODO: Support AJAX Uploads :-)
+ *
+ * @author Fabio Varesano <fvaresano at yahoo dot it>
+ * porting to Drupal 6
+ * @author Heshan Wanigasooriya <heshan at heidisoft.com><heshanmw@gmail.com>
+ * @todo
+*/
+
+/**
+ * Hide the node form and show the busy div
+*/
+Drupal.video_upload_hide = function () {
+  // hiding the form (using display: none) makes its file values empty in Konqueror (Possibly also Safari). So let's move the form away of the view of the browser
+
+  $('#node-form').css({ position: "absolute", top: "-4000px" });
+
+  $("#sending").show();
+  $("#video_upload_cancel_link").click(Drupal.video_upload_show);
+}
+
+Drupal.video_upload_show = function() {
+  $('#node-form').show();
+  $("#sending").hide();
+
+  //$("form").bind("submit", function() { return false; })
+  window.location = window.location;
+}
+
+/**
+ * Attaches the upload behaviour to the video upload form.
+ */
+Drupal.video_upload = function() {
+  $('#node-form').submit(Drupal.video_upload_hide);
+}
+
+// Global killswitch
+if (Drupal.jsEnabled) {
+  $(document).ready(Drupal.video_upload);
+}
