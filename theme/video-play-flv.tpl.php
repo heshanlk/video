4aa44298c200504a1d665a47720bc7112059067b
Dennis <illin@384543.no-reply.drupal.org>
2010-04-30 03:39:51 +0000

    Can now use swftools or flowplayer as a flashvideo player in the theme.

---
+++ theme/video-play-flv.tpl.php
@@ -0,0 +1,14 @@
+<?php 
+//$Id$
+/*
+ * @file
+ * Theme file to handle flv output.
+ * 
+ * Variables passed.
+ * $width is the width of the video.
+ * $height is the height of the video.
+ * $video is the URL of the actual video.
+ * $player output
+ */
+print $player;
+?>
