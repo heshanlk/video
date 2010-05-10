d5361453c988ae65755891de9a207e31d6f58d65
Dennis <illin@384543.no-reply.drupal.org>
2010-05-10 18:24:53 +0000

    added themed_output for player templates.  currently only flv is using this.  created new video_helper class to handle the video and thumbnail object.  Did some cleanup.

--- theme/video-play-flv.tpl.php
+++ theme/video-play-flv.tpl.php
@@ -5,9 +5,10 @@
  * Theme file to handle flv output.
  * 
  * Variables passed.
- * $video is the video output. Rendered from video_video_flv().
+ * $video is the video object.
  * $node is the node object.
+ * $themed_output is the rendered html.
  * 
  */
-print $video;
+print $themed_output;
 ?>
