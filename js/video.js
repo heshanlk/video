f86a62c9a637e4d8328e3628089b4fb9462dc037
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2011-12-21 14:10:47 +0100

    Issue #1356048 by Jorrit: Clean up JS and CSS

--- js/video.js
+++ js/video.js
@@ -1,58 +1,11 @@
-
-/**
- * @file
- * Adds some show/hide to the admin form to make the UXP easier.
- *
- */
-
 $(document).ready(function() {
   //lets see if we have any jmedia movies
   if($.fn.media) {
     $('.jmedia').media();
   }
 	
-  video_hide_all_options();
-  $("input[name='vid_convertor']").change(function() {
-    video_hide_all_options();
-  });
-
-  // change metadata options
-  video_hide_all__metadata_options();
-  $("input[name='vid_metadata']").change(function() {
-    video_hide_all__metadata_options();
-  });
-
-  // change metadata options
-  video_hide_all__filesystem_options();
-  $("input[name='vid_filesystem']").change(function() {
-    video_hide_all__filesystem_options();
-  });
-
-  $('.video_select').each(function() {
-    var ext = $(this).attr('rel');
-    $('select', this).change(function() {
-      if($(this).val() == 'video_play_flv') {
-        $('#flv_player_'+ext).show();
-      } else if($(this).val() == 'video_play_html5') {
-        $('#html5_player_'+ext).show();
-      } else {
-        $('#flv_player_'+ext).hide();
-        $('#html5_player_'+ext).hide();
-      }
-    });
-    if($('select', this).val() == 'video_play_flv') {
-      $('#flv_player_'+ext).show();
-    }
-    if($('select', this).val() == 'video_play_html5') {
-      $('#html5_player_'+ext).show();
-    } else {
-      $('#html5_player_'+ext).hide();
-    }
-  });
-	
   if(Drupal.settings.video) {
     $.fn.media.defaults.flvPlayer = Drupal.settings.video.flvplayer;
-
   }
 	
   //lets setup our colorbox videos
@@ -86,44 +39,3 @@ $(document).ready(function() {
   });
 });
 
-function video_hide_all_options() {
-  $("input[name='vid_convertor']").each(function() {
-    var id = $(this).val();
-    $('#'+id).hide();
-    if ($(this).is(':checked')) {
-      $('#' + id).show();
-    }
-  });
-}
-
-function videoftp_thumbnail_change() {
-  // Add handlers for the video thumbnail radio buttons to update the large thumbnail onchange.
-  $(".video-thumbnails input").each(function() {
-    var path = $(this).val();
-    if($(this).is(':checked')) {
-      var holder = $(this).attr('rel');
-      $('.'+holder+' img').attr('src', Drupal.settings.basePath + path);
-    }
-  });
-
-}
-
-function video_hide_all__metadata_options() {
-  $("input[name='vid_metadata']").each(function() {
-    var id = $(this).val();
-    $('#'+id).hide();
-    if ($(this).is(':checked')) {
-      $('#' + id).show();
-    }
-  });
-}
-
-function video_hide_all__filesystem_options() {
-  $("input[name='vid_filesystem']").each(function() {
-    var id = $(this).val();
-    $('#'+id).hide();
-    if ($(this).is(':checked')) {
-      $('#' + id).show();
-    }
-  });
-}
