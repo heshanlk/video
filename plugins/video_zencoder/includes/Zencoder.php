82e9daed94765ecc85bc7d73e2bb28ddc09717aa
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2012-01-11 09:10:24 +0100

    Issue #1399554 by Jorrit: Update Zencoder API to v2

--- plugins/video_zencoder/includes/Zencoder.php
+++ plugins/video_zencoder/includes/Zencoder.php
@@ -19,7 +19,7 @@ if(!function_exists('json_encode')) {
 
 class ZencoderJob {
 
-  var $new_job_url = "https://app.zencoder.com/api/jobs"; //https://app.zencoder.com/api/jobs
+  var $new_job_url = "https://app.zencoder.com/api/v2/jobs"; //https://app.zencoder.com/api/jobs
   //https://zencoder-staging.heroku.com/api/jobs
   var $new_job_params = array();
   var $new_job_json;
