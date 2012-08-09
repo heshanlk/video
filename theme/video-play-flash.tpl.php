7d60067c3e6458ae2e28b7f80afcd3224ef3818f
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2012-08-09 17:43:20 +0200

    Issue #1719062 by Jorrit: Fixed the SWF Flash player.

--- theme/video-play-flash.tpl.php
+++ theme/video-play-flash.tpl.php
@@ -18,10 +18,10 @@ if (!empty($item['thumbnailfile'])) {
 }
 ?>
 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="<?php print $width; ?>" height="<?php print $height; ?>" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0">
-  <param name="movie" value="<?php print file_create_url($video->files->{$video->player}->uri); ?>" />
+  <param name="movie" value="<?php print $url; ?>" />
   <param name="autoplay" value="<?php print $autoplay ? 'true' : 'false'; ?>" />
   <param name="wmode" value="transparent" />
-  <object class="video-object" type="application/x-shockwave-flash" data="<?php print file_create_url($video->files->{$video->player}->uri); ?>" width="<?php print $width; ?>" height="<?php print $height; ?>">
+  <object class="video-object" type="application/x-shockwave-flash" data="<?php print $url; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>">
     <?php print t('No video? Get the !plugin', array('!plugin' => l(t('Adobe Flash plugin'), url('http://get.adobe.com/flashplayer/')))); ?>
   </object>
 </object>
